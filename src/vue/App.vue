89544a487e696937872a1e49aaa421a6f12b7c1a
Eirik Torske <eirik.torske@gmail.com>
2024-11-19 09:10:43 +0100

    v0.4.0
    No styling / Default browser styling

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -10,26 +10,22 @@ const integrated = '✓'
 
 <template>
     <h1>{{ title }}</h1>
-    <p style="font-weight: bold;">Technology stack status:</p>
-    <ul style="list-style-type: none;">
-        <li>Git: <span style="font-size: larger;font-weight: bolder;color: green;">{{ integrated }}</span></li>
-        <li><a href="https://github.com/eirikt/default-webapp-vue-vite-vercel" target="_blank">GitHub</a>: <span style="font-size: larger;font-weight: bolder;color: green;">{{ integrated }}</span></li>
-        <li>GitHub: <span style="font-size: larger;font-weight: bolder;color: green;">{{ integrated }}</span></li>
-        <li>Vercel: <span style="font-size: larger;font-weight: bolder;color: green;">{{ integrated }}</span></li>
-        <li>Vite: <span style="font-size: larger;font-weight: bolder;color: green;">{{ integrated }}</span></li>
-        <li>Vue: <span style="font-size: larger;font-weight: bolder;color: green;">{{ integrated }}</span></li>
-        <li>TypeScript: <span style="font-size: larger;font-weight: bolder;color: green;">{{ integrated }}</span></li>
-        <li>Tailwind: <span style="font-size: larger;color: red;">{{ notIntegrated }}</span></li>
-        <li>Vitest: <span style="font-size: larger;color: red;">{{ notIntegrated }}</span></li>
-        <li>Pinia: <span style="font-size: larger;color: red;">{{ notIntegrated }}</span></li>
-        <li>HTTP APIs: <span style="font-size: larger;color: red;">{{ notIntegrated }}</span></li>
-        <li>TSC/ESLint: <span style="font-size: larger;color: red;">{{ notIntegrated }}</span></li>
+    <p>Technology stack status:</p>
+    <ul>
+        <li>Git: {{ integrated }}</li>
+        <li><a href="https://github.com/eirikt/default-webapp-vue-vite-vercel" target="_blank">GitHub</a>: {{ integrated }}</li>
+        <li>GitHub: {{ integrated }}</li>
+        <li>Vercel: {{ integrated }}</li>
+        <li>Vite: {{ integrated }}</li>
+        <li>Vue: {{ integrated }}</li>
+        <li>TypeScript: {{ integrated }}</li>
+        <li>Tailwind: {{ notIntegrated }}</li>
+        <li>Vitest: {{ notIntegrated }}</li>
+        <li>Pinia: {{ notIntegrated }}</li>
+        <li>HTTP APIs: {{ notIntegrated }}</li>
+        <li>TSC/ESLint: {{ notIntegrated }}</li>
     </ul>
-    <p style="padding-top: 8px;font-family: monospace;color: grey;">
-        [
-        v{{ version }} |
-        <a href="https://github.com/eirikt/default-webapp-vue-vite-vercel/blob/main/README.md"
-           style="font-family: monospace;color: grey;" target="_blank">Documentation</a>
-        ]
+    <p>
+        [ v{{ version }} | <a href="https://github.com/eirikt/default-webapp-vue-vite-vercel/blob/main/README.md" target="_blank">Documentation</a> ]
     </p>
 </template>
