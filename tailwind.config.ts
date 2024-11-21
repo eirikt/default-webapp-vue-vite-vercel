4e79f1765cc603d733ea1c3109ced8845da1f6d8
Eirik Torske <eirik.torske@gmail.com>
2024-11-21 08:43:27 +0100

    v0.4.3
    Tailwind's recommended font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,14 @@
 import type {Config} from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
     content: ['./src/vue/App.vue'],
     //plugins: [],
-    //theme: {extend: {}}
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+            }
+        }
+    }
 } satisfies Config
